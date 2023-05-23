b98483a2d73181252b8b4bacc513de41028e5cd6
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-05-23 20:10:04 +0600

    categories

--- index.js
+++ index.js
@@ -32,12 +32,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get("/categories/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const result = await toysCollection.findOne(query);
-            res.send(result);
-        });
+
 
         // all toys get into this
 
@@ -61,7 +56,12 @@ async function run() {
             }
         });
 
-
+        app.get("/alltoys/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
+            res.send(result);
+        });
 
         app.get("/mytoys", async (req, res) => {
             const email = req.query?.email;
